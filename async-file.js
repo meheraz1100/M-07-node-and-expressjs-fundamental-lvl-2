7f64df53726cfed3bdbf4380664ad8627f0c57e1
meheraz1100 <meheraz1100@gmail.com>
2025-01-31 19:49:33 +0600

    read & write data with sync and async

---
+++ async-file.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+
+// reading text asynchronously
+
+
+fs.readFile('./texts/read.txt', 'utf-8', (err, data) => {
+    if(err){
+        throw Error('Error reading text')
+    }
+
+
+    // writing text asynchronously
+
+    fs.writeFile('./texts/read-2.txt', data, 'utf-8', (err) => {
+        if(err){
+            throw Error('Error writing data')
+        }
+    })
+})
+
+console.log('testing async');
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const add = requi
+// local module
 
-add(2, 3)
+// const {a, add} = require('./local-1')
+// const {a: a2, add: add2} = require('./local-2')
+
+// console.log(add2(2,2,3));
+
+// built in module 
+const path = require("path")
+
+// console.log(path.dirname('/home/md-meheraz/Dropbox/Level2 Development/M-07-node-and-expressjs-fundamental/index.js'));
+// console.log(path.parse('/home/md-meheraz/Dropbox/Level2 Development/M-07-node-and-expressjs-fundamental/index.js'));
+// console.log(path.join('/home/md-meheraz/Dropbox/Level2 Development/M-07-node-and-expressjs-fundamental/', 'index.js'));
+console.log(path.win32('/home/md-meheraz/Dropbox/Level2 Development/M-07-node-and-expressjs-fundamental/', 'index.js'));
